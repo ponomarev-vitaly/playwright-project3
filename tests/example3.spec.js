c16c64d8541afa2968bd36290371dc8a99bf8909
ponomarev-vitaly <vp211082@gmail.com>
2024-10-26 23:32:50 -0500

    test: update example3.spec.js

--- tests/example3.spec.js
+++ tests/example3.spec.js
@@ -12,8 +12,13 @@ test.describe.parallel("Dropdown tests", () => {
   test("Test 1", async ({ page, browserName }) => {
     //
 
-    await page.locator("#dropdown").selectOption({"label": 'Option 1'});
-    await page.pause();
+    await page.locator('#dropdown').selectOption({"label": 'Option 1'});
+    // await page.pause();
+    await expect(page.locator('#dropdown')).toHaveValue('1');
+
+    await page.locator('#dropdown').selectOption({"label": 'Option 2'});
+    // await page.pause();
+    await expect(page.locator('#dropdown')).toHaveValue('2');
     
   });
 });
