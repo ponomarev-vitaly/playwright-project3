528b9918ba91793c09a85238cc0feebeadf5fa5a
ponomarev-vitaly <vp211082@gmail.com>
2024-10-26 23:20:45 -0500

    test: update example3.spec.js

---
+++ tests/example3.spec.js
@@ -0,0 +1,19 @@
+const { test, expect, firefox } = require("@playwright/test");
+
+test.describe.parallel("Dropdown tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/dropdown");
+  });
+
+  test.afterEach(async ({ page }) => {
+    console.log("This test is finished!");
+  });
+
+  test("Test 1", async ({ page, browserName }) => {
+    //
+
+    await page;
+    await page.pause();
+    
+  });
+});
