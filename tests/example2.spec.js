e2aa39b4335c644b308099fcba3f309cfb266393
ponomarev-vitaly <vp211082@gmail.com>
2024-10-26 23:16:35 -0500

    update 2 files and delete 2 files

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -1,6 +1,6 @@
 const { test, expect, firefox } = require("@playwright/test");
 
-test.describe.parallel("Checkboxes tests", () => {
+test.describe.parallel("Drag and Drop tests", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/drag_and_drop");
   });
@@ -13,6 +13,8 @@ test.describe.parallel("Checkboxes tests", () => {
     //
 
     await page.dragAndDrop('#column-a', '#column-b');
+    await page.pause();
+    await page.dragAndDrop('#column-b', '#column-a');
     await page.pause();
   });
 });
