1621fb0f0ae5f1b64d45843d8f81cb620d22e4c5
ponomarev-vitaly <vp211082@gmail.com>
2024-10-27 22:31:39 -0500

    test: update example6.spec.js

--- tests/example6.spec.js
+++ tests/example6.spec.js
@@ -11,8 +11,9 @@ test.describe.parallel("Upload files tests", () => {
 
   test("Test 1", async ({ page, browserName }) => {
     //
-    await page.setInputFiles('#file-upload', '/uploadedFiles/sample.pdf');
-    await page.locator("//input[@id='file-submit']").click();
+    await page.setInputFiles('#file-upload', 'uploadedFiles/sample.pdf');
+    await page.locator('input:has-text("Upload")').click();
+    
 
   });
 });
