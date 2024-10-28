507985dcec746a056424946501e9d9fad81be53c
ponomarev-vitaly <vp211082@gmail.com>
2024-10-27 21:55:12 -0500

    test: update example5.spec.js

--- tests/example5.spec.js
+++ tests/example5.spec.js
@@ -1,4 +1,4 @@
-const { test, expect } = require("@playwright/test");
+// const { test, expect } = require("@playwright/test");
 
 test.describe("Download files tests", () => {
   test.beforeEach(async ({ page }) => {
