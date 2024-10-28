b43ebf8c0f9fea0d0ab726a410e351dc2191d3af
ponomarev-vitaly <vp211082@gmail.com>
2024-10-27 22:41:59 -0500

    test: update example7.spec.js

---
+++ tests/example7.spec.js
@@ -0,0 +1,23 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe.parallel("Upload files tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/upload");
+  });
+
+  test.afterEach(async ({ page }) => {
+    console.log("This test is finished!");
+  });
+
+  test("Test 1", async ({ page, browserName }) => {
+    //
+    await page.setInputFiles('#file-upload', 'uploadedFiles/sample.pdf');
+    await page.locator('input:has-text("Upload")').click();
+
+    await expect(page.locator('text=File Uploaded!')).toBeVisible();
+
+    await expect(page.locator('text=sample.pdf')).toBeVisible();
+    
+
+  });
+});
