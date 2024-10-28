e91b45af42cf32d8d84ad5369574fd88dfba4ae5
ponomarev-vitaly <vp211082@gmail.com>
2024-10-27 21:36:56 -0500

    test: update example5.spec.js

---
+++ tests/example5.spec.js
@@ -0,0 +1,22 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Download files tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+  });
+
+  test.afterEach(async ({ page }) => {
+    console.log("This test is finished!");
+  });
+
+  test("Test 1", async ({ page, browserName }) => {
+    //
+    // Start waiting for download before clicking. Note no await.
+    const downloadPromise = page.waitForEvent("download");
+    await page.getByText("Download file").click();
+    const download = await downloadPromise;
+
+    // Wait for the download process to complete and save the downloaded file somewhere.
+    await download.saveAs("/path/to/save/at/" + download.suggestedFilename());
+  });
+});
