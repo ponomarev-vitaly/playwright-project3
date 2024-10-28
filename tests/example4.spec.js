694fb69235b5a30b0f015971d1bb5714ccc306f7
ponomarev-vitaly <vp211082@gmail.com>
2024-10-27 21:07:10 -0500

    test: update example4.spec.js

--- tests/example4.spec.js
+++ tests/example4.spec.js
@@ -11,9 +11,9 @@ test.describe.parallel("iFrames tests", () => {
 
   test("Test 1", async ({ page, browserName }) => {
     //
-    const frameTest = page.frameLocator('#mce_0_ifr').locator('html');
+    const frameTest = page.frameLocator('#mce_0_ifr').locator('body');
     await frameTest.click();
-    await frameTest.type("This is just a test typing if everuthing is working OK!");
+    await frameTest.type("body", "This is just a test typing if everuthing is working OK!");
 
         
     
