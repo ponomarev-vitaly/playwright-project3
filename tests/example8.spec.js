0a2a09ffd77d8b7f2cdd3fa9291a4d2409efcf05
ponomarev-vitaly <vp211082@gmail.com>
2024-10-28 23:55:22 -0500

    test: update example8.spec.js

---
+++ tests/example8.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Hovers tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/hovers");
+  });
+
+  test.afterEach(async ({ page }) => {
+    console.log("This test is finished!");
+  });
+
+  test("Test 1", async ({ page, browserName }) => {
+    //
+    
+  });
+});
