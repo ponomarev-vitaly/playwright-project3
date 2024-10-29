6cc229a87cc03534aafad71fa83aaa0478071dc5
ponomarev-vitaly <vp211082@gmail.com>
2024-10-29 17:10:26 -0500

    update 3 files and delete 4 files

--- tests/example8.spec.js
+++ tests/example8.spec.js
@@ -12,8 +12,8 @@ test.describe("Hovers tests", () => {
   test("Test 1", async ({ page, browserName }) => {
     //
     await page.hover('[alt="User Avatar"]');
-    await expect(page.getByText('text=name: user1')).toBeVisible;
-    await page.locator('text=View profile').click();
+    await expect(page.locator('text=name: user1')).toBeVisible();
+    await page.locator('text=View profile').first().click();
     await page.pause();
     
 
