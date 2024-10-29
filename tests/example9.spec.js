ffbef77d4b800717cb1cf30fbe38bcc9aa909501
ponomarev-vitaly <vp211082@gmail.com>
2024-10-29 17:34:46 -0500

    test: update example9.spec.js

---
+++ tests/example9.spec.js
@@ -0,0 +1,16 @@
+const { test, expect } = require("@playwright/test");
+
+test.describe("Key Presses tests", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/key_presses");
+  });
+
+  test.afterEach(async ({ page }) => {
+    console.log("This test is finished!");
+  });
+
+  test("Test 1", async ({ page, browserName }) => {
+    //
+    
+  });
+});
